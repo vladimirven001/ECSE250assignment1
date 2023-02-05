367843f5180f5117428d872d044f9e15922cdf1d
vladimirven001 <vladimir.venkov@mail.mcgill.ca>
2023-02-04 22:47:33 -0500

    flight reservation

--- src/assignment1/FlightReservation.java
+++ src/assignment1/FlightReservation.java
@@ -1,4 +1,42 @@
 package assignment1;
 
-public class FlightReservation {
+public class FlightReservation extends Reservation {
+
+    private Airport departure;
+    private Airport arrival;
+
+    public FlightReservation(String name, Airport departure, Airport arrival) {
+        
+        super(name);
+        this.departure = departure;
+        this.arrival = arrival;
+    
+        if (departure.equals(arrival)) {
+            throw new IllegalArgumentException("Departure and arrival airports are the same.");
+        }
+    }
+
+    public int getCost() {
+        
+        double planeFees = 5375;
+        double fuelCost = 124;
+        double fuelEfficiency = 167.52;
+
+        int dist = Airport.getDistance(departure, arrival);
+
+        return (int)Math.ceil((fuelCost * dist / fuelEfficiency) + planeFees + departure.getFees() + arrival.getFees());
+
+    }
+
+    public boolean equals(Object o) {
+
+        if (o instanceof FlightReservation && ((FlightReservation) o).reservationName() == this.reservationName() 
+            && ((FlightReservation) o).departure == this.departure && ((FlightReservation) o).arrival == this.arrival) {
+            return true;
+        }
+
+        return false;
+
+    }
+
 }
--- src/assignment1/Reservation.java
+++ src/assignment1/Reservation.java
@@ -1,4 +1,19 @@
 package assignment1;
 
-public class Reservation {
+public abstract class Reservation  {
+
+    private String name;
+
+    public Reservation(String name) {
+        this.name = name;
+    }
+
+    public final String reservationName() {
+        return this.name;
+    }
+
+    public abstract int getCost();
+    
+    public abstract boolean equals(Object o);
+
 }
